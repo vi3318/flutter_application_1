6c8db64ba79e855db0ee3640564cf728b245ccad
Vihaan Dharia <vihaandharia04@gmail.com>
2023-05-02 15:30:40 +0530

    Changes

---
+++ lib/homePage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  final int days = 30;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/homePage.dart';
 
-void main(){
-  runApp (MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("welcome to flutter")
+
+    return MaterialApp(
+      home: HomePage(),
     );
   }
 }
